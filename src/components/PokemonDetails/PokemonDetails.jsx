efe019a1d951b86b6ac7cfc998ccc9b0ff0afcc6
UmarAhmad12 <umara6274@gmail.com>
2023-08-20 23:07:51 +0530

    Added react route project

--- src/components/PokemonDetails/PokemonDetails.jsx
+++ src/components/PokemonDetails/PokemonDetails.jsx
@@ -1,8 +1,35 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
 function PokemonDetails (){
+    const {id} = useParams();
+    const [pokemon, setPokemon] = useState({});
+    async function downloadPokemon(){
+        const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`)
+        setPokemon({
+            name: response.data.name,
+            image: response.data.sprites.other.dream_world.front_default,
+            weight: response.data.weight,
+            height: response.data.height,
+            type: response.data.types.map((t)=> t.type.name)
+        })
+    }
+
+
+    useEffect(() => {
+        downloadPokemon();
+    }, []);
 return(
-    <>
-    Details
-    </>
+
+    <div className="pokemon-details-wrapper">
+    <div className="pokemon-details-name">name: {pokemon.name}</div>
+    <img className="pokemon-details-image" src={pokemon.image} />
+    <div>Height: {pokemon.height}</div>
+    <div>Weight: {pokemon.weight}</div>
+    <div className="pokemon-types">
+        {pokemon.types && pokemon.types.map((t) => <div key={t}> {t} </div>)}</div>
+    </div>
 )
 }
 
